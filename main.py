1dae8f314475f5195861df88f4b0d35e10c3e4b1
Aleksis Sprincis <aleksis.sprincis@gmail.com>
2026-09-03 20:02:51 +0300

    Add student management menu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from sqlalchemy import true
 from config import *
 
 connection = mysql.connector.connect(
@@ -8,4 +9,32 @@ connection = mysql.connector.connect(
     database=DB_NAME
 )
 
-print("Connected to MySQL")
+cursor = connection.cursor()
+
+while true:
+    print("\n1. Add Student")
+    print("2. View Students")
+    print("3. Exit")
+
+    choice = input("Choose: ")
+
+    if choice == "1":
+        name = input("Enter student name: ")
+        email = input("Enter student email: ")
+        age = input("Enter student age: ")
+
+        sql = """
+        INSERT INTO students (name, email, age) VALUES (%s, %s, %s) """
+
+        cursor.execute(sql, (name, email, age))
+        connection.commit()
+        print("Student added successfully!")
+
+    elif choice == "2":
+        cursor.execute("SELECT * FROM students")
+    
+        for student in cursor.fetchall():
+            print (student)
+
+    elif choice == "3":
+        break
